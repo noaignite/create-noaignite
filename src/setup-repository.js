88473f807ceb5d3edb670779355687f70375ae9e
Milica Spasenovic <milica.spasenovic@noaignite.com>
2025-06-26 15:59:27 +0200

    ci: src

--- src/setup-repository.js
+++ src/setup-repository.js
@@ -12,17 +12,22 @@ export const setupRepository = async (projectName) => {
     }
  
     execCommand(`npx create-turbo@latest .`, { cwd: projectPath });
-  
-    execCommand('npm create sanity@latest -- --dataset production --template clean --typescript --output-path ./apps/studio', { cwd: projectPath })
-
-    // To do soon
-    // execCommand(`git clone --depth=1 ${TEMPLATE_REPO_URL} ${projectPath}/apps/studio`);
-    // fs.rmSync(path.join(`${projectPath}/apps/studio`, '.git'), { recursive: true });
-    
+    console.log(" ")
+    console.log("---------------------------> done! (npx create-turbo@latest)")
+    console.log(" ")
     execCommand('npx @noaignite/create-app', { cwd: projectPath });
-    // execCommand('npm install', { cwd: `${projectPath}/apps/studio` });
+    console.log(" ")
+    console.log("---------------------------> done! (npx @noaignite/create-app)")
+    console.log(" ")
+    execCommand('npm create sanity@latest -- --dataset production --template clean --typescript --output-path ./apps/studio', { cwd: projectPath })
+    console.log(" ")
+    console.log("---------------------------> done! SANITY")
+    console.log(" ")
     execCommand('git init', { cwd: projectPath });
     execCommand('git add .', { cwd: projectPath });
     execCommand('git commit -m "Initial commit"', { cwd: projectPath });
+    console.log(" ")
+    console.log("---------------------------> done! GIT")
+    console.log(" ")
     console.log("Don't forget to publish to GitHub later when you are ready :)")
 };
